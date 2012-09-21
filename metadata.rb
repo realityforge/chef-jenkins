d9cb07788a6b2d25dddd0fa3e648783a490385fe
Peter Donald <peter@realityforge.org>
2012-09-21 10:41:38 +1000

    Note that cookbook does not managed slaves

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
  maintainer       "AJ Christensen"
 maintainer_email "aj@junglist.gen.nz"
 license          "Apache 2.0"
-description      "Installs and configures Jenkins CI server & slaves"
+description      "Installs and configures Jenkins CI server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.6.5"
 
