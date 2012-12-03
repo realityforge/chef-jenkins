231c7fb59bb3033ac115caf120201fd1922cd248
Peter Donald <peter@realityforge.org>
2012-12-03 15:31:23 +1100

    Remove whitespace

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,5 @@ description      "Installs and configures Jenkins CI server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.6.7"
 
- depends 'java'
- recommends 'authbind'
+depends 'java'
+recommends 'authbind'
