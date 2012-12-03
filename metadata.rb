2e0719adbaa11727cbde5c52b420e52bddedf1cf
Peter Donald <peter@realityforge.org>
2012-12-03 15:31:56 +1100

    Replace " with ' characters

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-maintainer       "Peter Donald"
-maintainer_email "peter@realityforge.org"
-license          "Apache 2.0"
-description      "Installs and configures Jenkins CI server"
+maintainer       'Peter Donald'
+maintainer_email 'peter@realityforge.org'
+license          'Apache 2.0'
+description      'Installs and configures Jenkins CI server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.6.7"
+version          '0.6.8'
 
 depends 'java'
 recommends 'authbind'
